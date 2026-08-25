e9ed616028c2fc19ed1b9e02c6ececc942c3e169
Lithin Adari <157233883+LithinAdari@users.noreply.github.com>
2026-08-25 18:06:34 +0530

    modified file

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -9,9 +9,6 @@ import { createOnRampTransaction } from "../app/lib/actions/createOnRamptxn";
 
 
 
-
-
-
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
     redirectUrl: "https://netbanking.hdfcbank.com"
